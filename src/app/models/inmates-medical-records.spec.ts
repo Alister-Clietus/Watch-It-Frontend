bfa29e61fb6760e4383d237984e1ca74617875ba
Alister-Clietus <knp19cs011@ceknpy.ac.in>
2025-02-16 16:22:36 +0530

    added new components

---
+++ src/app/models/inmates-medical-records.spec.ts
@@ -0,0 +1,7 @@
+import { InmatesMedicalRecords } from './inmates-medical-records';
+
+describe('InmatesMedicalRecords', () => {
+  it('should create an instance', () => {
+    expect(new InmatesMedicalRecords()).toBeTruthy();
+  });
+});
---
+++ src/app/models/inmates-medical-records.ts
@@ -0,0 +1,8 @@
+export class InmatesMedicalRecords {
+    id?: number;
+    name: string = '';
+    latestDoctorResult: File | null = null;
+    bloodResult: File | null = null;
+    dietPlan: File | null = null;
+  }
+  
